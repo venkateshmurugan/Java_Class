9ad65e68e9fbade150dfed9590fcf06b02559482
venkateshmurugan <murugan.venkatesh@gmail.com>
2022-11-23 15:43:42 +0000

    11/23/2022

--- ConditionsDemo.java
+++ ConditionsDemo.java
@@ -1,26 +1,48 @@
 class ConditionsDemo{
     public static void main(String[] args){
 
-        for(int j = 0; j < args.length; ++j)
+        for(int j = 0;
+         j < args.length;
+          ++j)
+          {
             System.out.println(" " + args[j]);
+          }
 
+        //Nested If
         if(args.length > 0){
-            Integer i = Integer.parseInt(args[0]);
+            Integer mark = Integer.parseInt(args[1]);
             
-            if(i > 60){
-                System.out.println("The student got first class");
+            if(mark > 60){
+                System.out.println("The student got first class in " + args[0]);
 
-                if(i > 75){
-                System.out.println("The student got distinction class");    
+                if(mark > 75){
+                System.out.println("The student got distinction class in " + args[0]);    
                 }
             }
             else{
-                System.out.println("The student did not get first class");
+                System.out.println("The student did not get first class in " + args[0]);
 
-                if(i < 45){
-                System.out.println("The student did not pass");    
+                if(mark < 45){
+                System.out.println("The student did not pass in " + args[0]);    
                 }
             }
+
+        //If else Ladder
+        if(mark  < 45){
+            System.out.println("The student did not earn any badge in " + args[0]);   
+        }
+        else if((mark  >= 45) && (mark <= 60)){
+            System.out.println("The student earned a white badge in " + args[0]);   
+        }
+        else if((mark  > 60) && (mark <= 75)){
+            System.out.println("The student earned a Yellow badge in " + args[0]);   
+        }
+        else if((mark  > 75)  && (mark <= 90)){
+            System.out.println("The student earned a Green badge in " + args[0]);   
+        }
+        else{
+            System.out.println("The student earned a Black badge in " + args[0]);
+        }
 
         }
         System.out.println("This is the end of the program");
