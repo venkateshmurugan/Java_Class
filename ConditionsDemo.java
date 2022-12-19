d76a96a4943a88722937274b0b8cbe7efd3b7908
venkateshmurugan <murugan.venkatesh@gmail.com>
2022-12-19 15:35:09 +0000

    Conditions File

--- ConditionsDemo.java
+++ ConditionsDemo.java
@@ -1,22 +1,41 @@
 class ConditionsDemo{
-    public static void main(String[] args){
+    public static void main(String[] args){ //command line argument or command line parameter
 
-        for(int j = 0;
-         j < args.length;
-          ++j)
+        for(int j = 0; j < args.length; ++j)
           {
             System.out.println(" " + args[j]);
           }
 
+        //java ConditionsDemo 60 70 80 90 100
+        //args
+        //{60, 70, 80}
+        //args[0] 60
+        //args[1] 70
+        // args[2] 80
+
+
+        //java ConditionsDemo English 90
+        //java ConditionsDemo
+
+        //Sting[] num = new String[2];
+        //num[0] = "English";
+        //num[1] = "90";
+
+        //String[] num;
+
+        //args {"English", "90"}
+        int x = 1;
+        int y = 0;
+
         //Nested If
         if(args.length > 0){
-            Integer mark = Integer.parseInt(args[1]);
+            Integer mark = Integer.parseInt(args[x]);
             
             if(mark > 60){
-                System.out.println("The student got first class in " + args[0]);
+                System.out.println("The student got first class in " + args[y]);
 
                 if(mark > 75){
-                System.out.println("The student got distinction class in " + args[0]);    
+                    System.out.println("The student got distinction class in " + args[0]);    
                 }
             }
             else{
